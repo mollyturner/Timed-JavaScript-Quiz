2c4251f49e2304863cacf330d39a285d0cd80260
mollyturner <mollyturner50@gmail.com>
2023-02-04 01:33:54 -0600

    Add final score page and timer/score functions

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,30 @@
-// Populate questions, pop answers,
+// Page on load
 var topOfPage = document.createElement('div');
 var highScEl = document.createElement('span');
 var time = document.createElement('span');
+var secondSection = document.createElement('section');
 var h1El = document.createElement('h1');
 var descriptEl = document.createElement('div')
 var startBtnDiv = document.createElement('div');
 var startBtn = document.createElement('button');
+
+// Question elements
 var quizCard = document.getElementById('quiz-card');
 var titleSection = document.getElementById('title-section');
+var questionResult = document.createElement('div');
 
-// Questions
+// High scores page
+var goBackEl = document.createElement('button');
+var clearHighScoresEl = document.createElement('button');
+
+// High Scores page
+
+
+//global var being incremented to change question object 
+var questionNum = 0;
+var runTimer = true;
+
+// Question elements
 var qList = document.createElement('ul');
 var q1 = document.createElement('li');
 var q2 = document.createElement('li');
@@ -17,43 +32,57 @@ var q3 = document.createElement('li');
 var q4 = document.createElement('li');
 var q5 = document.createElement('li');
 
+// Assigning content to load page
 highScEl.textContent = 'View High Scores';
 time.textContent = 'Time: 0';
 quizCard.style.display = 'none';
-
 h1El.textContent = 'Coding Quiz Challenge';
 descriptEl.textContent = 'Try to answer the following code-related questions within the time limit.  Keep in mind that incorrect answers will penalize your score/time by ten seconds!';
 startBtn.textContent = 'Start Quiz'
 
+// Adding score/time to first section
 titleSection.appendChild(topOfPage)
 topOfPage.appendChild(highScEl);
 topOfPage.appendChild(time);
 
-titleSection.appendChild(h1El);
-titleSection.appendChild(descriptEl);
-titleSection.appendChild(startBtnDiv);
+// Adding quiz title, description and 'start quiz' button to second section
+titleSection.append(secondSection);
+secondSection.appendChild(h1El);
+secondSection.appendChild(descriptEl);
+secondSection.appendChild(startBtnDiv);
 startBtnDiv.append(startBtn);
+quizCard.after(questionResult);
 
+// Adding ids to first page
 topOfPage.setAttribute('id', 'highScoreDiv');
 highScEl.setAttribute('id', 'highScores');
 time.setAttribute('id', 'timeCounter');
 h1El.setAttribute('id', 'title');
 descriptEl.setAttribute('id', 'gameDescript');
 startBtnDiv.setAttribute('id', 'firstBtnDiv');
+secondSection.setAttribute('id', 'section2');
 
-startBtn.addEventListener('click', () => {questionRender(0)});
+questionResult.setAttribute('style', 'font-size: x-large; color: gray; text-align: center; margin-top: 20px;');
 
+// Adding event listener on click for timer countdown
+startBtn.addEventListener('click', function (event) {
+    secondSection.style.display = 'none';
+    questionNum = 0;
+    questionRender(questionNum);
+    countdown();
+});
+var timeRemaining = 75;
 function countdown() {
-    var timeRemaining = 5;
     var timeInterval = setInterval(function () {
-        if (timeRemaining > -1) {
+        if (timeRemaining > -1 && runTimer) {
             time.textContent = 'Time: ' + timeRemaining;
             timeRemaining--;
         }
     }, 1000);
 }
-countdown();
 
+
+// Quiz questions array including options and answers
 var questionsArray = [
     {
         answer: "alerts",
@@ -90,25 +119,23 @@ var questionsArray = [
     },
 ];
 
-//global var being incremented to change question object 
-var questionNum = 0;
-
-//calls functions to delete listeners, render next question and delete time
-function wrongAnswer() {          
-    deleteListeners(); 
+// Wrong answer function - deletes listeners, render next question, subtracts time, notifies answer incorrect
+function wrongAnswer() {
+    deleteListeners();
     questionRender(questionNum);
-    //TODO: make this function delete time
-    console.log('Wrong answer');
+    timeRemaining -= 5;
+    questionResult.textContent = 'Wrong!';
 };
 
-//calls functions to delete listeners, render next question
-function correctAnswer() {        
-    deleteListeners();         
+// Correct answer function - deletes listeners, render next question, notifies answer correct
+function correctAnswer() {
+    deleteListeners();
     questionRender(questionNum);
     console.log('correct answer');
+    questionResult.textContent = 'Correct!';
 };
 
-//gets called before rendering the next question to delete the previous event listeners
+// Call before rendering the next question to delete the previous event listeners
 function deleteListeners() {
     for (let index = 1; index < 5; index++) {
         console.log(`Deleting listener ${index}`)
@@ -121,44 +148,97 @@ function deleteListeners() {
 }
 
 
-//Checks the options and renders buttons depending on if the answer is correct or not
-function answerCheck (questionObj) {
+// Checks options and renders buttons depending on if answer is correct or not
+function answerCheck(questionObj) {
+    console.log(`questionObj ${JSON.stringify(questionObj)}`);
     for (let index = 0; index < questionObj.options.length; index++) {
-
-
         if (questionObj.options[index] == questionObj.answer) {
-            document.getElementById(`answer-btn-${[index + 1]}`)
+            document.getElementById(`answer-btn-${index + 1}`)
                 .addEventListener('click', correctAnswer);
         } else {
-            document.getElementById(`answer-btn-${[index + 1]}`)
+            document.getElementById(`answer-btn-${index + 1}`)
                 .addEventListener('click', wrongAnswer);
         };
+
+        console.log(`index ${index}`);
     };
+
+    console.log(`questionNum ${questionNum + 1}`);
+    console.log(`questionsArray ${questionsArray.length}`);
+
+    if (questionNum + 1 === questionsArray.length) {
+        console.log('last question');
+
+        runTimer = false;
+
+        quizCard.style.display = 'none';
+
+        createScorePage();
+    }
 }
 
-//Renders the current question to the page
+
+
+function createScorePage() {
+    var questionResult = document.createElement('div');
+    var h2El = document.createElement('h2');
+    var completedEl = document.createElement('div');
+    var initialsParentDiv = document.createElement('div');
+    var initialsEl = document.createElement('div');
+    var inputBox = document.createElement('input');
+    var submitBtn = document.createElement('input');
+
+    questionResult.setAttribute('id', 'questionResultID');
+    initialsParentDiv.setAttribute('id', 'initialsParentID');
+    submitBtn.setAttribute('type', 'submit');
+    submitBtn.setAttribute('value', 'Submit');
+    submitBtn.setAttribute('id', 'finalSubmit');
+    inputBox.setAttribute('id', 'inputBoxID');
+
+    h2El.textContent = 'All done!';
+    completedEl.textContent = `Your final score is ${timeRemaining+1}`;
+    initialsEl.textContent = 'Enter Initials: ';
+
+    highScoreDiv.after(questionResult);
+    questionResult.appendChild(h2El);
+    questionResult.appendChild(completedEl);
+    questionResult.appendChild(initialsParentDiv);
+    initialsParentDiv.appendChild(initialsEl);
+    initialsParentDiv.appendChild(inputBox);
+    initialsParentDiv.appendChild(submitBtn);
+
+}
+
+// Renders the current question to the page
 function questionRender(number) {
     quizCard.style.display = 'flex';
-    
-    //calls the answer check function to correct the event listener
+
+    // Calls the answer check function to correct the event listener
     answerCheck(questionsArray[questionNum]);
 
-    //renders the question to the page
+    // Renders the question to the page
     var question = document.getElementById('quiz-question');
     question.innerText = questionsArray[questionNum].question;
 
-    //for loop to print the options into the button depending on what questionsArray object you are on
+    // for loop to print the options into the button depending on what questionsArray object you are on
     for (let index = 0; index < questionsArray[questionNum].options.length; index++) {
         var button = document.getElementById(`answer-btn-${[index + 1]}`)
 
         button.innerText = questionsArray[questionNum].options[index];
     }
 
-    //this checks to see if the number is 0 and sets it to 1 or increments it if it isnt. it changes a global var "questionNum"
+    // Checks to see if the number is 0 and sets it to 1 or increments it if it isnt. it changes a global var "questionNum"
     if (number = 0) {
         questionNum = 1;
     } else {
         questionNum = questionNum + 1;
     }
-    
 };
+
+submitBtn.addEventListener('click', function () {
+    questionResultID.style.display = 'none';
+    topOfPage.style.display = 'none';
+})
+
+
+
